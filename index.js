1f9da9fa69411c41b12b212e41edd9a042711229
liavksz <alicia_s_souza@estudante.sesisenai.org.br>
2026-07-09 16:50:53 -0300

    Add average calculation and approval check

---
+++ index.js
@@ -0,0 +1,13 @@
+let nota1 = 8;
+let nota2 = 7;
+let nota3 = 9;
+
+let media = (nota1 + nota2 + nota3) / 3;
+
+console.log("A média é: " + media);
+
+if (media >= 7) {
+    console.log("Aluno aprovado");
+} else {
+    console.log("Aluno reprovado");
+}
